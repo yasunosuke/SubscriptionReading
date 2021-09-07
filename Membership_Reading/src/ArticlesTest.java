dcdea8aecab765835a4f0f8857aa68036f546157
yasuhiro <yasuhiro@yasunosuke>
2021-09-07 13:03:30 +0900

    test: New test method testGetInstance() @ArticlesTest
    Tested if the Singleton works as expected

--- Membership_Reading/src/ArticlesTest.java
+++ Membership_Reading/src/ArticlesTest.java
@@ -5,8 +5,15 @@ import org.junit.jupiter.api.Test;
 class ArticlesTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetInstance() throws Exception {
+		Articles article1 = Articles.getInstance();
+		Articles article2 = Articles.getInstance();
+		
+		assertSame(article2, article1);
 	}
+	
+	
+	
+	
 
 }
