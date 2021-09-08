8878750346c9b17b45a6eb7705e9e3188adadebd
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2021-09-08 09:25:37 +0900

    revert:

--- Membership_Reading/src/WriterTest.java
+++ Membership_Reading/src/WriterTest.java
@@ -1,12 +1,32 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.Test;
 
 class WriterTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testWriter() throws Exception {
+		String name = "a";
+		
+		Writer w = new Writer(name);
+		
+		assertEquals(name, getWriterName(w));
+		
+	}
+	
+//	Writerクラスの特定のフィールドを取ってくる
+	Field getWriterField(String fieldName) throws Exception {
+		Field f = Writer.class.getDeclaredField(fieldName);
+		f.setAccessible(true);
+		return f;
+	}
+	
+//	指定されたインスタンスのnameの値をとってくる
+	String getWriterName(Writer obj) throws Exception {
+		Field f = getWriterField("name");
+		return (String) f.get(obj);
 	}
 
 }
