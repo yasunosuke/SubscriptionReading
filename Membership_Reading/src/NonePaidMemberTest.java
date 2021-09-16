5f72830c7ccba61aa8a766cb2954ef7f00fbab5e
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2021-09-16 11:58:41 +0900

    feat: add some vacant test methods @NonePaidMember.java

---
+++ Membership_Reading/src/NonePaidMemberTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NonePaidMemberTest {
+
+	@Test
+	public void testNonePaidMember() throws Exception {
+		
+	}
+	
+	@Test
+	void test() throws Exception {
+		
+	}
+
+}
