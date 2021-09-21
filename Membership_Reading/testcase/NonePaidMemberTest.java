671aac620b35988e5683bbc6c79dc3b5ab383e42
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2021-09-21 09:45:39 +0900

    teat, feat: Made new test method @NonePaidMemberTest
    To test name field made and used Reflection

--- Membership_Reading/testcase/NonePaidMemberTest.java
+++ Membership_Reading/testcase/NonePaidMemberTest.java
@@ -1,13 +1,34 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.Test;
 
 class NonePaidMemberTest {
 
 	@Test
 	public void testNonePaidMember() throws Exception {
+		Articles articles = Articles.getInstance();
+		Member npm = new NonePaidMember(articles);
 		
+		assertEquals("ゲスト", getMemberName(npm));
+		assertEquals(articles, )
 	}
+	
+	 //	Hogeクラスの特定のフィールドを取ってくる
+		Field getMemberField(String fieldName) throws Exception {
+			Field f = Member.class.getDeclaredField(fieldName);
+			f.setAccessible(true);
+			return f;
+		}
+		
+	     //	指定されたインスタンスのsomethingをとってくる
+		String getMemberName(Member obj) throws Exception {
+			Field f = getMemberField("name");
+			return (String) f.get(obj);
+		}
+		
+		
 	
 	@Test
 	void test() throws Exception {
