254030e0d913f65b819a62d98dfcd78594bfdf84
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2021-09-21 15:44:55 +0900

    test, refactor: Nest some methods by class that test instanciating @NonePaidMemberTest

--- Membership_Reading/testcase/NonePaidMemberTest.java
+++ Membership_Reading/testcase/NonePaidMemberTest.java
@@ -3,53 +3,65 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.lang.reflect.Field;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.experimental.runners.Enclosed;
 
-class NonePaidMemberTest {
-
-//	name fieldが適切に初期化されているか確かめるためのメソッド。
-	@Test
-	public void testNonePaidMember() throws Exception {
-		
-		Articles articles = Articles.getInstance();
-		Member npm = new NonePaidMember(articles);
-		
-		assertEquals("ゲスト", getMemberName(npm));
-	}
+@RunWith(Enclosed.class)
+public class NonePaidMemberTest {
 	
-	 //	Memberクラスの特定のフィールドを取ってくる
+	public class InstanciatingTest {
+		
+		//name fieldが適切に初期化されているか確かめるためのメソッド。
+		@Test
+		public void testNonePaidMember() throws Exception {
+
+			Articles articles = Articles.getInstance();
+			Member npm = new NonePaidMember(articles);
+
+			assertEquals("ゲスト", getMemberName(npm));
+		}
+
+		//Memberクラスの特定のフィールドを取ってくる
 		Field getMemberField(String fieldName) throws Exception {
 			Field f = Member.class.getDeclaredField(fieldName);
 			f.setAccessible(true);
 			return f;
 		}
-		
-	     //	指定されたインスタンスのnameをとってくる
+
+		//指定されたインスタンスのnameをとってくる
 		String getMemberName(Member obj) throws Exception {
 			Field f = getMemberField("name");
 			return (String) f.get(obj);
 		}
-		
-// TODO articles field に初期化されたシングルトンと事前に取得したシングルトンが一致するか確かめる	
-	@Test
-	void test() throws Exception {
-		
-		Articles articles = Articles.getInstance();
-		NonePaidMember npm = new NonePaidMember(articles);
-		
-		assertEquals(articles, getNonePaidMemberArticles(npm));
-	}
-	
-//	指定されたインスタンスのarticlesをとってくる
-	Articles getNonePaidMemberArticles(Member obj) throws Exception {
-		Field f = getNonePaidMemberField("articles");
-		return (Articles) f.get(obj);
-	}
-	
-//	NonePaidMemberクラスの特定のフィールドを取ってくる
+
+		//articles field にArticlesクラスのインスタンスが期待通りに初期化されたかを確かめるためのメソッド	
+		@Test
+		void test() throws Exception {
+
+			Articles articles = Articles.getInstance();
+			NonePaidMember npm = new NonePaidMember(articles);
+
+			assertEquals(articles, getNonePaidMemberArticles(npm));
+		}
+
+		//指定されたインスタンスのarticlesをとってくる
+		Articles getNonePaidMemberArticles(Member obj) throws Exception {
+			Field f = getNonePaidMemberField("articles");
+			return (Articles) f.get(obj);
+		}
+
+		//NonePaidMemberクラスの特定のフィールドを取ってくる
 		Field getNonePaidMemberField(String fieldName) throws Exception {
 			Field f = NonePaidMember.class.getDeclaredField(fieldName);
 			f.setAccessible(true);
 			return f;
 		}
+	}
+	
+	public class readTest {
+		
+	}
+
+
 
 }
